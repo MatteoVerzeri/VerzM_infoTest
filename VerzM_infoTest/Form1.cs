ff35b617e2a30166a18e901e82b80abafc31fc78
MatteoVerzeri <matverzer@gmail.com>
2023-03-27 10:58:47 +0200

    task 5

--- VerzM_infoTest/Form1.cs
+++ VerzM_infoTest/Form1.cs
@@ -12,14 +12,67 @@ namespace VerzM_infoTest
 {
     public partial class Form1 : Form
     {
+        List<VerzM_Candidato> candidati;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
+        }
+        public void aggiunta(VerzM_Candidato candidato)
+        {
+            candidati.Add(candidato);
+        }
+        public string[] visualizza()
+        {
+            
+            int x = candidati.Count;
+            string[] c = new string[x];
+            int cont = 0;
+            foreach(VerzM_Candidato candidato in candidati)
+            {
+                c[cont] = candidato.ToString();
+                cont++;
+            }
+            return c;
+        }
+        public void modifica(string nome, int matricola)
+        {
+            for(int i=0; i < candidati.Count; i++)
+            {
+                if(candidati[i].VerzM_Matricola == matricola)
+                {
+                    candidati[i].VerzM_Nome = nome;
+                }
+            }
+        }
+        public void elimina(int matricola)
+        {
+            candidati.RemoveAt(matricola);
+        }
+        public string[] visualizzaidonei()
+        {
+            int x = candidati.Count;
+            string[] c = new string[x];
+            int cont = 0;
+            foreach (VerzM_Candidato candidato in candidati)
+            {
+                if (candidato.isIdoneo() == true)
+                {
+                    c[cont] = candidato.ToString();
+                }
+                cont++;
+            }
+            return c;
+        }
+        public void ordinadecrscente()
+        { 
+            candidati.Sort();
+            candidati.Reverse(); 
         }
     }
 }
