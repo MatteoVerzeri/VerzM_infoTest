6e860f129d65f473a7290fe06843c797cf7d340d
MatteoVerzeri <matverzer@gmail.com>
2023-03-27 08:33:43 +0200

    Task1

--- VerzM_infoTest/Form1.cs
+++ VerzM_infoTest/Form1.cs
@@ -16,5 +16,10 @@ namespace VerzM_infoTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ VerzM_infoTest/VerzM_Candidato.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzM_infoTest
+{
+    public abstract class VerzM_Candidato
+    {
+        private int VerzM_matricola;
+        private string VerzM_nome;
+        public int VerzM_Matricola { get; set; }
+        public string VerzM_Nome { get; set; }
+        public VerzM_Candidato()
+        {
+            VerzM_matricola = 0;
+            VerzM_Nome = "";
+        }
+        public VerzM_Candidato(int VerzM_matricola)
+        {
+            this.VerzM_matricola = VerzM_matricola;
+            VerzM_Nome = "";
+        }
+        public VerzM_Candidato(string VerzM_nome)
+        {
+            this.VerzM_nome = VerzM_nome;
+            VerzM_matricola = 0;
+        }
+        public VerzM_Candidato(int VerzM_matricola, string VerzM_nome)
+        {
+            this.VerzM_Matricola = VerzM_matricola;
+            this.VerzM_Nome = VerzM_nome;
+        }
+        public abstract bool isIdoneo();
+        public abstract int punteggio();
+    }
+}
